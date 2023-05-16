7d6796298d48cd37b4620f29850b207048658b72
KacperMadajewski <kacpermadajewski@gmail.com>
2023-05-16 20:50:14 +0200

    Styles for exercise 1

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,26 +1,25 @@
+import styles from './profile.module.css'
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div>
-      <div>
-        <img
-          src={avatar}
-          alt="User avatar"
-        />
+    <div className={styles.background}>
+      <div className={styles.avatar}>
+        <img src={avatar} alt="User avatar" />
         <p>{username}</p>
         <p>{tag}</p>
         <p>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={styles.stats}>
+        <li className={styles.Components}>
           <span>Followers</span>
           <span>{stats.followers}</span>
         </li>
-        <li>
+        <li className={styles.Components}>
           <span>Views</span>
           <span>{stats.views}</span>
         </li>
-        <li>
+        <li className={styles.Components}>
           <span>Likes</span>
           <span>{stats.likes}</span>
         </li>
